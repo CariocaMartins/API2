eb8baaf98d4c0a69befd228808103535e1131e88
natanmartins0908@gmail.com <natanmartins0908@gmail.com>
2023-04-02 16:27:13 -0300

    Adicionar arquivos de projeto.

---
+++ API2/Controllers/FatorialController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FatorialController : ControllerBase
+    {
+        [HttpGet("{numero}")]
+        public IActionResult Get(int numero)
+        {
+            if (numero < 0)
+            {
+                return BadRequest("O número não pode ser negativo");
+            }
+
+            int resultado = 1;
+            for (int i = 2; i <= numero; i++)
+            {
+                resultado *= i;
+            }
+
+            return Ok(resultado);
+        }
+    }
+}
